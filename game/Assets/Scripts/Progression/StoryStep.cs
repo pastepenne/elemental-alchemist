ddca2daafeabf085e143b481269710b5d8aadb63
Irina <estodificilo@gmail.com>
2026-06-18 01:00:25 +0300

    Added more element sources

--- game/Assets/Scripts/Progression/StoryStep.cs
+++ game/Assets/Scripts/Progression/StoryStep.cs
@@ -1,5 +1,6 @@
 using ElementalAlchemist.Dialogue;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace ElementalAlchemist.Progression
 {
@@ -8,15 +9,15 @@ namespace ElementalAlchemist.Progression
     {
         [SerializeField] private string _triggerId;
         [SerializeField] private string _objectiveText;
-        [SerializeField] private string _waypointKey;
-        [SerializeField] private string _approachWaypointKey;
+        [SerializeField] private string _waypointKey; 
+        [SerializeField] private string _warpWaypointKey;
         [SerializeField] private DialogueData _dialogue;
 
         public string TriggerId => _triggerId;
         public string ObjectiveText => _objectiveText;
         public string WaypointKey => _waypointKey;
 
-        public string WarpWaypointKey => _approachWaypointKey;
+        public string WarpWaypointKey => _warpWaypointKey;
         public DialogueData Dialogue => _dialogue;
     }
 }
